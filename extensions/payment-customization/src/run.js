6c813c0c44dee92f27fbb39c1dc07dd20277e9a0
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2024-02-07 18:36:44 +0530

    use metafields to provide the configuration values to the input function

--- extensions/payment-customization/src/run.js
+++ extensions/payment-customization/src/run.js
@@ -17,21 +17,33 @@ const NO_CHANGES = {
  * @returns {FunctionRunResult}
  */
 export function run(input) {
-  console.error('input', input)
+  console.error('input', JSON.stringify(input))
+
+  const configuration = JSON.parse(input.paymentCustomization.metafield?.value ?? "{}")
+  console.error('configuration', JSON.stringify(configuration));
+
+  //return if no configuration
+  if(!configuration.paymentMethodName || !configuration.cartTotal){
+    return NO_CHANGES
+
+  }
+
+
 
   const cartTotal = parseFloat(input.cart.cost.totalAmount.amount ?? "0.0")
-  if(cartTotal < 100){
-    console.error('cartTotal is less than 100')
+  if(cartTotal < configuration.cartTotal){
+    console.error('No Change - cartTotal is less than', configuration.cartTotal)
     return NO_CHANGES
   } 
-  console.error('cartTotal is greater than or equal to 100')
-  const hidePaymentMethod = input.paymentMethods.find(method => method.name.includes('Cash on Delivery'))
+  console.error('cartTotal is greater than or equal to ', configuration.cartTotal)
+
+  const hidePaymentMethod = input.paymentMethods.find(method => method.name.includes(configuration.paymentMethodName))
   if (!hidePaymentMethod) {
-    console.error('No payment method found with name "Cash on Delivery"')
+    console.error('No payment method found with name ', configuration.paymentMethodName)
     return NO_CHANGES;
   }
 
-  console.error('Payment method found with name Cash on Delivery, will be hidden')
+  console.error('Payment method found with name', configuration.paymentMethodName,', will be hidden')
 
 
   return {
